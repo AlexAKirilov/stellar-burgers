9410a95ac2f0ff4df1a37b4208baeacc7b22f8a0
AlexAKirilov <apple.kirilov2013@gmail.com>
2025-05-15 21:37:36 +0500

    feat: add routing and authorization

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -1,4 +1,11 @@
 import { FC } from 'react';
 import { AppHeaderUI } from '@ui';
+import { useSelector } from '../../services/store';
+import { UserSelector } from '../../services/userSlice';
 
-export const AppHeader: FC = () => <AppHeaderUI userName='' />;
+// TODO: Update the `AppHeader` component to dynamically fetch and display user data from the Redux store using `useSelector` and `UserSelector`.
+
+export const AppHeader: FC = () => {
+  const user = useSelector(UserSelector.userDataSelector);
+  return <AppHeaderUI userName={user ? `${user?.name}` : ''} />;
+};
